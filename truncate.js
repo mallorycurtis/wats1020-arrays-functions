ace371392cdfb945873a841f166e7fa6b5e7c9e4
Mallory Curtis <mallorycurtis@outlook.com>
2015-07-03 11:05:50 -0700

    Upload!!

--- truncate.js
+++ truncate.js
@@ -1,6 +1,6 @@
 // This is the file where you will write the Truncate Words function and related code.
 
-// TODO: Create a Function called truncateWords() that accepts two arguments: 
+// TODO: Create a Function called truncateWords() that accepts two arguments:
 // longText (a String with several words in it)
 // numWords (an Integer that sets the number of words you want in the returned text)
 
@@ -14,10 +14,19 @@
 // 7. Return the truncated String from the Function
 
 // This portion of the script is meant to call and display the result of your Function.
-// You do not need to change the following lines, but you may change them if you 
+// You do not need to change the following lines, but you may change them if you
 // are experimenting further or pursuing stretch goals.
 var originalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
 var wordLimit = 8;
 var shortText = truncateWords(originalText, wordLimit);
 console.log('originalText: ' + originalText);
 console.log('shortText: ' + shortText);
+
+function truncateWords() {
+  var longText = originalText.split(" ");
+  //split the string into an array
+  var numWords = longText.splice (0, wordLimit);
+  //instead of finding the length and subtracting, I thought the splice function would do the same thing in less steps
+  console.log (numWords.join(" ") + "...");
+  //join the function back into a string and add ellipses in
+}
